45cbc85cb2cb46ad1aaa8a9e8c7179b66986c173
Robert "Archy" Hardy <hardyrar@gmail.com>
2022-08-14 17:53:59 +0100

    Add MovingPlatform class and Rider integration

---
+++ ObstacleAssault/Intermediate/Build/Win64/ObstacleAssaultEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -0,0 +1,2 @@
+// Compiler: 14.33.31629
+#include "C:/Users/hardy/_dev/gamedev/ObstacleAssault/ObstacleAssault/Intermediate/Build/Win64/ObstacleAssaultEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
